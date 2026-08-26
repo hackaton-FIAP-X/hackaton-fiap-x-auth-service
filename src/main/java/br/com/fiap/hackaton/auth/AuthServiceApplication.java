9a2edc7fa366d2bd6e0d2b3476eab8bc99bd265a
denis rodrigues <denisrodrigues.ita@gmail.com>
2026-08-26 19:52:51 -0300

    first commit

---
+++ src/main/java/br/com/fiap/hackaton/auth/AuthServiceApplication.java
@@ -0,0 +1,14 @@
+package br.com.fiap.hackaton.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthServiceApplication.class, args);
+  }
+}
+// Hot reload test Wed Aug 26 18:52:05 -03 2026
+// Hot reload test Wed Aug 26 19:23:23 -03 2026
---
+++ src/test/java/br/com/fiap/hackaton/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.fiap.hackaton.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
